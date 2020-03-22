e113af0a471833e88ec10bf9925b82ee92d6a385
Patricia Bere <d00193593@student.dkit.ie>
2020-03-22 19:32:36 +0000

    Latest Update - Patricia
    - Added menu scene back
    - Added glow and particles (improved)

---
+++ First Prototype/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button play_button, setting_button, exit_button; //button gameObjects
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        //when buttons are clicked
+        play_button.onClick.AddListener(playGame);
+        setting_button.onClick.AddListener(OpenSettingsMenu);
+        exit_button.onClick.AddListener(exitGame);
+    }
+
+    void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void OpenSettingsMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    void exitGame()
+    {
+        Application.Quit();
+    }
+}
