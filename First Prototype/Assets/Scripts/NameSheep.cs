e1f93d4f0e7a6ff13fc61772e0596fb5ab196ac6
KerrieC98 <38563192+KerrieC98@users.noreply.github.com>
2020-03-25 11:05:33 +0000

    Added test scene for naming sheep

---
+++ First Prototype/Assets/Scripts/NameSheep.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameSheep : MonoBehaviour
+{
+
+    public string name;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        name = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Sheep Name: " + name;
+    }
+}
