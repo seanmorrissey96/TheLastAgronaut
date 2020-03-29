f4836535c87b25d287d571f597070002a606770a
KerrieC98 <38563192+KerrieC98@users.noreply.github.com>
2020-03-29 18:39:55 +0100

    Implemented GPS's opening dialogue and fixed a bug with some sheep ui not appearing

--- First Prototype/Assets/Scripts/GPSPatrol.cs
+++ First Prototype/Assets/Scripts/GPSPatrol.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class GPSPatrol : MonoBehaviour
 {
@@ -12,9 +13,13 @@ public class GPSPatrol : MonoBehaviour
     public Transform[] moveSpots;
     private int patrollingTo;
 
+    public GameObject gpsDialoguePanel;
+    public Text dialogue1;
+
     NavMeshAgent _navMeshAgent;
     void Start()
     {
+        //dialogue1.GetComponent<Text>();
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
         waitTime = startWaitTime;
         patrollingTo = 0;
@@ -26,15 +31,22 @@ public class GPSPatrol : MonoBehaviour
         _navMeshAgent.SetDestination(targetVector);
         //transform.position = Vector3.MoveTowards(transform.position, moveSpots[patrollingTo].position, speed * Time.deltaTime);
         print("Distance to other: " + Vector3.Distance(transform.position, moveSpots[patrollingTo].position));
+        print("Patrolling to: " + patrollingTo);
+        if (patrollingTo >= 3)
+        {
+            dialogue1.text = "Some of the sheep seem a little down, go see if you can't cheer them up!";
+            StartCoroutine("WaitForSec");
+        }
+
         if (Vector3.Distance(transform.position, moveSpots[patrollingTo].position) < 0.5f)
         {
-            if(waitTime <= 0)
+            if (waitTime <= 0)
             {
-                if(patrollingTo < moveSpots.Length - 1)
+                if (patrollingTo < moveSpots.Length - 1)
                 {
                     patrollingTo++;
                 }
-                
+
                 waitTime = startWaitTime;
             }
             else
@@ -42,5 +54,11 @@ public class GPSPatrol : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(6);
+        gpsDialoguePanel.SetActive(false);
     }
 }
