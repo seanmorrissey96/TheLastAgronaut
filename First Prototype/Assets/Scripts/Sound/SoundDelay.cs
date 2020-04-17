0f7119b7b86e159a071a820c581d3a2e5192129d
Sean <seanmorrissey96@gmail.com>
2020-04-17 19:24:17 +0100

    Penelope's Intro Dialogue
    An intro audio clip of Penelope's dialogue.

---
+++ First Prototype/Assets/Scripts/Sound/SoundDelay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDelay : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float delayTime;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Invoke("playAudio", delayTime);
+    }
+
+    private void playAudio()
+    {
+        audioSource.Play();
+    }
+}
